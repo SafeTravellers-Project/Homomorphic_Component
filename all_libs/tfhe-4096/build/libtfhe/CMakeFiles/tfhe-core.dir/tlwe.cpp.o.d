cb4d2bbbbf7396ba4ec7389e7cdb32d54d8e0dcb
oliverRHUL <olivebpilani@gmail.com>
2025-09-02 15:55:20 +0200

    tfhe added instead of submodule

---
+++ all_libs/tfhe-4096/build/libtfhe/CMakeFiles/tfhe-core.dir/tlwe.cpp.o.d
@@ -0,0 +1,25 @@
+libtfhe/CMakeFiles/tfhe-core.dir/tlwe.cpp.o: \
+ /home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/libtfhe/tlwe.cpp \
+ /usr/include/stdc-predef.h \
+ /home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/include/tfhe_core.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/include/tfhe_generic_templates.h \
+ /home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/include/tlwe.h \
+ /home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/include/tfhe_core.h \
+ /home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/include/lweparams.h \
+ /home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/include/polynomials.h
---
+++ all_libs/tfhe-4096/build/libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/libtfhe/fft_processors/spqlios/lagrangehalfc_impl_fma.s" "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/build/libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/lagrangehalfc_impl_fma.s.o"
+  "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/libtfhe/fft_processors/spqlios/spqlios-fft-fma.s" "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/build/libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/spqlios-fft-fma.s.o"
+  "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/libtfhe/fft_processors/spqlios/spqlios-ifft-fma.s" "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/build/libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/spqlios-ifft-fma.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/libtfhe/fft_processors/spqlios/fft_processor_spqlios.cpp" "libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/fft_processor_spqlios.cpp.o" "gcc" "libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/fft_processor_spqlios.cpp.o.d"
+  "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/libtfhe/fft_processors/spqlios/lagrangehalfc_impl.cpp" "libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/lagrangehalfc_impl.cpp.o" "gcc" "libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/lagrangehalfc_impl.cpp.o.d"
+  "/home/olivechakraborty/Documents/CEA/Projects/SAFETravellers/Codes/Project25Update/tfhe-4096/src/libtfhe/fft_processors/spqlios/spqlios-fft-impl.cpp" "libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/spqlios-fft-impl.cpp.o" "gcc" "libtfhe/fft_processors/spqlios/CMakeFiles/tfhe-fft-spqlios-fma.dir/spqlios-fft-impl.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
